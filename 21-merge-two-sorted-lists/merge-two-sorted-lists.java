ce31297aed07173a190a28089c7d9adca9f3de8e
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-01-29 13:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (19.32%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,91 +10,48 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // if(list1 == null){
-        //     return list2;
-        // }
-        // if(list2 == null){
-        //     return list1;
-        // }
-        // // the head of merged list
-        // ListNode head;
-
-        // if(list1.val <= list2.val){
-        //     head = list1;
-        //     list1 = list1.next;
-        // }
-        // else{
-        //     head = list2;
-        //     list2 = list2.next;
-        // }
-        
-        // // initalise node of traversing through merged array
-        // ListNode temp = head;
-
-        // while(list1 != null && list2 != null){
-        //     if(list1.val <= list2.val){
-        //         temp.next = list1;
-        //         list1 = list1.next;
-        //     }
-        //     else{
-        //         temp.next = list2;
-        //         list2 = list2.next;
-        //     }
-        //     temp = temp.next;
-        // }
-
-        // while(list1 != null){
-        //     temp.next = list1;
-        //     list1 = list1.next;
-        // }
-        // while(list2 != null){
-        //     temp.next = list2;
-        //     list2 = list2.next;
-        // }
-
-        // // return head of merged list
-        // return head;
-        
-        
-        
-        if (list1 == null) {
+        if(list1 == null){
             return list2;
         }
-        if (list2 == null) {
+        if(list2 == null){
             return list1;
         }
+        // the head of merged list
+        ListNode head;
 
-        ListNode head; // The head of the merged list
-
-        if (list1.val <= list2.val) {
+        if(list1.val <= list2.val){
             head = list1;
             list1 = list1.next;
-        } else {
+        }
+        else{
             head = list2;
             list2 = list2.next;
         }
+        
+        // initalise node of traversing through merged array
+        ListNode temp = head;
 
-        ListNode current = head; // Current node in the merged list
-
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                current.next = list1;
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                temp.next = list1;
                 list1 = list1.next;
-            } else {
-                current.next = list2;
+            }
+            else{
+                temp.next = list2;
                 list2 = list2.next;
             }
-            current = current.next; // Move current to the next node in the merged list
+            temp = temp.next;
         }
 
-        // If any of the lists is not exhausted, append the remaining nodes
-        if (list1 != null) {
-            current.next = list1;
+        if(list1 != null){
+            temp.next = list1;
         }
-        if (list2 != null) {
-            current.next = list2;
+        if(list2 != null){
+            temp.next = list2;
         }
 
-        return head; // Return the merged list head
+        // return head of merged list
+        return head;
+
     }
 }
