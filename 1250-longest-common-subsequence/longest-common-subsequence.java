6b51faf659440800d6639349758329a05ccef4aa
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-02-19 20:06:37 +0530

    Time: 63 ms (5.01%) | Memory: 69.1 MB (5.06%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -4,25 +4,23 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length(), n = text2.length();
         
-        // Initialize the dp array
-        ArrayList<ArrayList<Integer>> dp = new ArrayList<>();
-        for (int i = 0; i <= m; i++) {
-            ArrayList<Integer> row = new ArrayList<>(Collections.nCopies(n + 1, 0));
-            dp.add(row);
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        for(int i=0;i<=m;i++){
+            ArrayList<Integer> arr = new ArrayList<>(Collections.nCopies(n+1,0));
+            res.add(arr);
         }
-        
-        // Fill the dp array
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp.get(i).set(j, dp.get(i - 1).get(j - 1) + 1);
-                } else {
-                    dp.get(i).set(j, Math.max(dp.get(i - 1).get(j), dp.get(i).get(j - 1)));
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    res.get(i).set(j, res.get(i-1).get(j-1) + 1);
+                }
+                else{
+                    res.get(i).set(j, Math.max(res.get(i - 1).get(j), res.get(i).get(j - 1)));
+
                 }
             }
         }
-        
-        // Return the result
-        return dp.get(m).get(n);
+        return res.get(m).get(n);
     }
 }
