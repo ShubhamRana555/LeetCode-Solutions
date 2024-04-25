ebef63d7c5ba705601ffc79cdd688d7fc48ace02
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-04-25 17:17:38 +0530

    Time: 10 ms (20.52%) | Memory: 45 MB (95.18%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.java
@@ -0,0 +1,81 @@
+class MyLinkedList {
+    class ListNode {
+        ListNode next;
+        int data;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    private ListNode head;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) return -1;
+
+        ListNode temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.data;
+    }
+
+    public void addAtHead(int val) {
+        ListNode newNode = new ListNode(val);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    public void addAtTail(int val) {
+        ListNode newNode = new ListNode(val);
+        if (head == null) {
+            head = newNode;
+        } else {
+            ListNode temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        size++;
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+
+        if (index == 0) {
+            addAtHead(val);
+        } else {
+            ListNode newNode = new ListNode(val);
+            ListNode temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+            size++;
+        }
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) return;
+
+        if (index == 0) {
+            head = head.next;
+        } else {
+            ListNode temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+        }
+        size--;
+    }
+}
