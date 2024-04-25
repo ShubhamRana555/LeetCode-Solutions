86250022b3c93d391cd9afc62c16ac09557b0849
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-04-25 16:13:02 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (18.08%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -13,9 +13,11 @@ class Solution {
         ListNode curr = head;
         while(curr!=null && curr.next!=null){
             if(curr.val == curr.next.val){
+                // don't increment from current pointer until duplicates are found
                 curr.next = curr.next.next;
             }
             else{
+                // increment pointer as curr and curr.next are unique
                 curr = curr.next;
             }
         }
