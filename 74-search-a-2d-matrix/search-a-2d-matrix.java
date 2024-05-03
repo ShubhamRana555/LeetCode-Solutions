73d859560df7562b8a99939001a7920216a548d3
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-05-03 05:41:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (33.68%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
-            return false;
-        }
         
         int n = matrix.length;
         int m = matrix[0].length;
