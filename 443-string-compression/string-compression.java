3f3134918c399afc9ecde9e56a6695b1191d5b94
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-05-12 22:01:10 +0530

    Time: 2 ms (32.74%) | Memory: 43.7 MB (67.35%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -4,23 +4,20 @@ class Solution {
         int ptr1 = 0, ptr2 = 0;
         
         for(int i = 0; i < n; i++) {
-            int count = 1;
+            Integer count = 1; // Using Integer instead of int
             ptr2 = i;
-            // Count the number of repeated characters
             while(i < n - 1 && chars[i] == chars[i + 1]) {
                 count++;
                 i++;
             }
-            // Copy the character
             chars[ptr1++] = chars[ptr2];
-            // If count > 1, convert count to characters
             if(count > 1) {
-                String countStr = String.valueOf(count);
+                String countStr = count.toString(); // Convert Integer to String
                 for(char c : countStr.toCharArray()) {
                     chars[ptr1++] = c;
                 }
             }
         }
-        return ptr1; // Return the length of the compressed array
+        return ptr1;
     }
 }
