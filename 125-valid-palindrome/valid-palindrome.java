8289a9b560b4b69d9146623d1d8639f6185c88c3
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-05-13 22:03:27 +0530

    Time: 4 ms (55.51%) | Memory: 43 MB (60.29%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,27 +1,28 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str="";
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)>='a' && s.charAt(i)<='z' || s.charAt(i)>='A' && s.charAt(i)<='Z'){
-                str += Character.toLowerCase(s.charAt(i));
+        StringBuilder str1 = new StringBuilder("");
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+                str1.append(s.charAt(i));
+
             }
-            else if(s.charAt(i)>='0' && s.charAt(i)<='9'){
-                str += s.charAt(i);
+            if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
+                str1.append(Character.toLowerCase(s.charAt(i)));
+
+            }
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                str1.append(s.charAt(i));
+
             }
         }
-            System.out.println(str);
-
-        int start = 0, end = str.length()-1;
-        while(start <= end){
-            if(str.charAt(start) != str.charAt(end)){
+        int n = str1.length();
+        for(int i=0;i<n/2;i++){
+            if(str1.charAt(i) != str1.charAt(n-i-1)){
                 return false;
             }
-            else{
-                start++;
-                end--;
-            }
         }
-
         return true;
+        
     }
 }
