ad9bb4168d19d2386482e13a4d41396b559f74fe
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-05-13 23:36:37 +0530

    Time: 21 ms (58.13%) | Memory: 44.8 MB (56.78%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,35 +1,46 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        // if (s == null || s.length() < 1) return "";
         int n = s.length();
-        int start = 0, end = 1;
-        int low, hi;
 
-        for (int i = 0; i < n; i++) {
-            // for even palindromic substring
-            low = i - 1;
-            hi = i;
-            while (low >= 0 && hi < n && s.charAt(low) == s.charAt(hi)) {
-                if (hi - low + 1 > end) {
-                    start = low;
-                    end = hi - low + 1;
-                }
-                low--;
-                hi++;
-            }
-            // for odd palindromic substring
-            low = i - 1;
-            hi = i + 1;
-            while (low >= 0 && hi < n && s.charAt(low) == s.charAt(hi)) {
-                if (hi - low + 1 > end) {
-                    start = low;
-                    end = hi - low + 1;
-                }
-                low--;
-                hi++;
-            }
+        if(s.length() <= 1){
+            return s;
         }
 
-        return s.substring(start, start + end);
+        String lps = "";
+        
+        for(int i=1; i<s.length(); i++){
+            //consider odd length
+            int low = i;
+            int high = i;
+            while(s.charAt(low) == s.charAt(high)){
+                low--;
+                high++;
+
+                if(low == -1 || high == s.length()){
+                    break;
+                }
+            } 
+
+            String palindrome = s.substring(low+1, high);
+            if(palindrome.length() > lps.length()){
+                lps = palindrome;
+            }
+
+            // consider even length
+            low = i-1;
+            high = i;
+
+            while(s.charAt(low) == s.charAt(high)){
+                low--;
+                high++;
+                if(low == -1 || high == s.length()) break;
+            }
+            palindrome = s.substring(low+1, high);
+
+            if(palindrome.length() > lps.length()){
+                lps = palindrome;
+            }
+        }
+        return lps;
     }
 }
