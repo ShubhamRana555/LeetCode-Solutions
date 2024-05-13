4b735d8cf0674de6490dc9039ca3c8ce96c0d63b
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-05-13 09:29:06 +0530

    Time: 7 ms (66.40%) | Memory: 42.5 MB (92.86%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String reverseWords(String s) {
+        // Remove leading and trailing whitespaces and split the string by one or more whitespaces
+        String[] words = s.trim().split("\\s+");
+        
+        // Reverse the order of words
+        int i = 0, j = words.length - 1;
+        while (i < j) {
+            String temp = words[i];
+            words[i] = words[j];
+            words[j] = temp;
+            i++;
+            j--;
+        }
+        
+        // Join the reversed words with a single whitespace between them
+        return String.join(" ", words);
+    }
+}
+    
