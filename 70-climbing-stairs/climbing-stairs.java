96612c128fff435bc2917d98cdb75874b8667baa
Shubham Rana <131574171+ShubhamRana555@users.noreply.github.com>
2024-07-28 23:21:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (72.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,18 +1,23 @@
+import java.util.Arrays;
+
 class Solution {
     public int climbStairs(int n) {
-        ArrayList<Integer>dp = new ArrayList(Collections.nCopies(n+1,-1));
-        return helper(n,dp);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
     }
-    private int helper(int n,ArrayList<Integer>dp){
-        if(n<0)
-        return 0;
-        if(n == 0 || n==1){
-            return 1;
+
+    private int helper(int n, int[] dp) {
+        if (n == 1 || n == 2) {
+            return n;
         }
-        if(dp.get(n) != -1) return dp.get(n);
-        int w1 = helper(n-1,dp);
-        int w2 = helper(n-2,dp);
-        dp.set(n,w1+w2);
-        return dp.get(n);
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int oneWay = helper(n - 1, dp);
+        int twoWay = helper(n - 2, dp);
+
+        dp[n] = oneWay + twoWay;
+        return dp[n];
     }
 }
